cf3776ac827911663a42fd119953c05fe1b40676
Neha Gupta <nehaguptang.ng@gmail.com>
2020-10-31 21:42:13 +0530

    Update Shell_sort.cpp

--- Sorting/Shell_sort.cpp
+++ Sorting/Shell_sort.cpp
@@ -15,8 +15,7 @@ void display(int *array, int size) {
 }
 void shellSort(int *arr, int n) {
    int gap, j, k;
-   for(gap = n/2; gap > 0; gap = gap / 2) {        //initially gap = n/2,
-      decreasing by gap /2
+   for(gap = n/2; gap > 0; gap = gap / 2) {        //initially gap = n/2, decreasing by gap /2
       for(j = gap; j<n; j++) {
          for(k = j-gap; k>=0; k -= gap) {
             if(arr[k+gap] >= arr[k])
