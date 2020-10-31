ce576b938d37df604deae5eccb3ea36afc624747
ABHINAV  SANTOSH PANDEY <45191400+ABHI2598@users.noreply.github.com>
2020-10-31 10:28:14 +0530

    Update Swapper.cpp

--- Swapper.cpp
+++ Swapper.cpp
@@ -5,9 +5,9 @@ int main()
     int a = 5, b = 10, temp;
     cout << "Before swapping." << endl;
     cout << "a = " << a << ", b = " << b << endl;
-    temp = a;
-    a = b;
-    b = temp;
+    a = a+b;
+    b = a-b;
+    a = a-b;
     cout << "\nAfter swapping." << endl;
     cout << "a = " << a << ", b = " << b << endl;
     return 0;
